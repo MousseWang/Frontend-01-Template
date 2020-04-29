48f886b060dc4bc9300ba9bc7b0e15c09bf467da
wangyao <710075242@qq.com>
2020-04-30 00:30:35 +0800

    week3

--- week03/StringToNumber.js
+++ week03/StringToNumber.js
@@ -3,7 +3,7 @@
  * @Author: wyao
  * @Date: 2020-04-29 23:19:01
  * @LastEditors: wyao
- * @LastEditTime: 2020-04-30 00:02:18
+ * @LastEditTime: 2020-04-30 00:28:22
  */
 
 
@@ -15,17 +15,30 @@ function StringToNumber(str, x){
 
   let number = 0;
 
+  let numE = 0;
+
   let i = 0;
 
   let fac = 1;
 
   while(i < chars.length && chars[i] !== '.'){
-    number = number * x
-    number += chars[i].codePointAt(0) - '0'.codePointAt(0);
-    i++;
+    if(chars[i] === 'e'){
+      numE = number;
+      number = 0;
+      i++
+    }else{
+      number = number * x
+      number += chars[i].codePointAt(0) - '0'.codePointAt(0);
+      i++;
+    }
   }
 
   chars[i] === '.' && i++;
+
+  if(numE !== 0){
+    console.log(numE)
+    number = numE * Math.pow(10,number)
+  }
 
   while(i < chars.length){
     fac = fac / x;
