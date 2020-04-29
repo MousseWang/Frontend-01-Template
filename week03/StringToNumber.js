ae41884322f8604f1628730274b6590d92dda707
wangyao <710075242@qq.com>
2020-04-30 00:02:50 +0800

    week3

--- week03/StringToNumber.js
+++ week03/StringToNumber.js
@@ -3,7 +3,7 @@
  * @Author: wyao
  * @Date: 2020-04-29 23:19:01
  * @LastEditors: wyao
- * @LastEditTime: 2020-04-29 23:56:33
+ * @LastEditTime: 2020-04-30 00:02:18
  */
 
 
@@ -21,11 +21,7 @@ function StringToNumber(str, x){
 
   while(i < chars.length && chars[i] !== '.'){
     number = number * x
-    if(x > 10 && chars[i].codePointAt(0) > 97){
-      number += chars[i].codePointAt(0) - 'a'.codePointAt(0) + 10;
-    }else{
-      number += chars[i].codePointAt(0) - '0'.codePointAt(0);
-    }
+    number += chars[i].codePointAt(0) - '0'.codePointAt(0);
     i++;
   }
 
